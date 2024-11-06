f20e6ecbfe9de92b47ae7f81b2b36b43bb4cfa7a
Juho Kemppainen <jhmkemppainen16@gmail.com>
2024-11-06 11:34:02 +0200

    Complete 12

---
+++ viikko2/nhl-reader/src/player_stats.py
@@ -0,0 +1,8 @@
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.players = player_reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        filtered_players = filter(lambda player: player.nationality == nationality, self.players)
+        sorted_players = sorted(filtered_players, key=lambda player: player.points, reverse=True)
+        return sorted_players
